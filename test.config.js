1c23bf74865c94a97b38b1f38a7907884370ad63
mennei <menachems@webtech-inv.co.il>
2019-06-18 11:39:24 +0300

    Clean code

--- test.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Edit the value of targetAttribute with the
- * common attribute in your DOM that is given to the elements
- * you want the Autmation to target. 
- */
-module.exports = {
-    targetAttribute: 'data-menora-bo'
-};
